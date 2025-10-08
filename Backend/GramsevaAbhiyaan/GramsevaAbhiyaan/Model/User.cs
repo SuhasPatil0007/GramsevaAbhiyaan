f1b0e2b73bc243cc211c8d8cadadbeb598596702
DESKTOP-1G1A5EV\Suhas Patil <suhasmdpatil@gmail.com>
2025-10-08 13:34:32 +0530

    Main Models created as per project requirement and done with Initial Migration along with connection string and DbContext setup

--- Backend/GramsevaAbhiyaan/GramsevaAbhiyaan/Model/User.cs
+++ Backend/GramsevaAbhiyaan/GramsevaAbhiyaan/Model/User.cs
@@ -1,6 +1,15 @@
-﻿namespace GramsevaAbhiyaan.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GramsevaAbhiyaan.Model
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
